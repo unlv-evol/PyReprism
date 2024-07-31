c439e6abab1d95d76a36fa8f59e227e98c48103d
Daniel <danielogen@gmail.com>
2024-07-30 21:05:48 -0700

    ft: flake8 more config

--- src/PyReprism/languages/yaml.py
+++ src/PyReprism/languages/yaml.py
@@ -9,33 +9,31 @@ class Yaml:
     @staticmethod
     def file_extension() -> str:
         return extension.yaml
-    
+
     @staticmethod
     def keywords() -> list:
-         keyword = ''.split('|')
-        
-         return keyword
-    
+        keyword = ''.split('|')
+        return keyword
+
     @staticmethod
     def comment_regex():
         pattern = re.compile(r'(?P<comment>#.*?$)|(?P<noncomment>[^#\n]*[^\n]*)', re.MULTILINE)
         return pattern
 
-    
     @staticmethod
     def number_regex():
         pattern = re.compile(r'')
         return pattern
-    
+
     @staticmethod
     def operator_regex():
         pattern = re.compile(r'')
         return pattern
-    
+
     @staticmethod
     def keywords_regex():
         return re.compile(r'\b(' + '|'.join(Yaml.keywords()) + r')\b')
-    
+
     @staticmethod
     def remove_comments(source_code: str, isList: bool = False) -> str:
         result = []
@@ -46,8 +44,7 @@ class Yaml:
             return result
         return ''.join(result)
 
-
     @staticmethod
     def remove_keywords(source: str):
-        return re.sub(re.compile(Yaml.keywords_regex()), '', source)
-    
+        pattern = re.sub(re.compile(Yaml.keywords_regex()), '', source)
+        return pattern
--- src/PyReprism/utils/normalizer.py
+++ src/PyReprism/utils/normalizer.py
@@ -6,13 +6,10 @@ class Normalizer:
         pass
 
     @staticmethod
-    def whitespaces_regex ():
+    def whitespaces_regex():
         return re.compile(r'[\t\x0b\x0c\r ]+')
-    
+
     @staticmethod
-    def remove_whitespaces (source: str):
-        return re.sub(Normalizer.whitespaces_regex(), '', source)
-
-
-
-
+    def remove_whitespaces(source: str):
+        pattern = re.sub(Normalizer.whitespaces_regex(), '', source)
+        return pattern
