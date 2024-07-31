6805b263597c6ff13dd4fbadedc1692d6094d7d1
Daniel <danielogen@gmail.com>
2024-07-30 21:05:37 -0700

    ft: flake8 more config

--- src/PyReprism/languages/typescript.py
+++ src/PyReprism/languages/typescript.py
@@ -9,13 +9,12 @@ class TypeScript:
     @staticmethod
     def file_extension() -> str:
         return extension.typescript
-    
+
     @staticmethod
     def keywords() -> list:
-         keyword = 'as|async|await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|set|static|super|switch|this|throw|try|typeof|var|void|while|with|yield|module|declare|constructor|namespace|abstract|require|type|string|Function|any|number|boolean|Array|symbol|console'.split('|')
-        
-         return keyword
-    
+        keyword = 'as|async|await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|set|static|super|switch|this|throw|try|typeof|var|void|while|with|yield|module|declare|constructor|namespace|abstract|require|type|string|Function|any|number|boolean|Array|symbol|console'.split('|')
+        return keyword
+
     @staticmethod
     def comment_regex():
         pattern = re.compile(r'(?P<comment>//.*?$|/\*[\s\S]*?\*/|/\*.*?$|^.*?\*/|/\*\*[\s\S]*?\*/)|(?P<noncomment>\'(\\.|[^\\\'])*\'|"(\\.|[^\\"])*"|.[^/\'"]*)', re.DOTALL | re.MULTILINE)
@@ -25,16 +24,16 @@ class TypeScript:
     def number_regex():
         pattern = re.compile(r'\b0b[01]+\b|\b0x[\da-f]*\.?[\da-fp-]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?[df]?')
         return pattern
-    
+
     @staticmethod
     def operator_regex():
         pattern = re.compile(r'(^|[^.])(?:\+[+=]?|-[-=]?|!=?|<<?=?|>>?>?=?|==?|&[&=]?|\|[|=]?|\*=?|\/=?|%=?|\^=?|[?:~])')
         return pattern
-    
+
     @staticmethod
     def keywords_regex():
         return re.compile(r'\b(' + '|'.join(TypeScript.keywords()) + r')\b')
-    
+
     @staticmethod
     def remove_comments(source_code: str, isList: bool = False) -> str:
         result = []
@@ -45,8 +44,6 @@ class TypeScript:
             return result
         return ''.join(result)
 
-
     @staticmethod
     def remove_keywords(source: str):
         return re.sub(re.compile(TypeScript.keywords_regex()), '', source)
-    
