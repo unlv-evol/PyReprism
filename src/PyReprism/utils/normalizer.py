076ae77485e3bc87ec65bf4fb5e6c29f1fef06b5
Daniel <danielogen@gmail.com>
2024-07-31 08:24:19 -0700

    hotfix: removing whitespaces should also remove blank lines

--- src/PyReprism/utils/normalizer.py
+++ src/PyReprism/utils/normalizer.py
@@ -7,9 +7,9 @@ class Normalizer:
 
     @staticmethod
     def whitespaces_regex():
-        return re.compile(r'[\t\x0b\x0c\r ]+')
+        return re.compile(r'[\t\x0b\x0c\r ]+|^\s*\n', re.MULTILINE)
 
     @staticmethod
     def remove_whitespaces(source: str):
-        pattern = re.sub(Normalizer.whitespaces_regex(), '', source)
+        pattern = re.sub(Normalizer.whitespaces_regex(), '', source).strip()
         return pattern
