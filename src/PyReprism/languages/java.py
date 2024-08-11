1eefbbce90b6aca57d10dce360be958492e33281
Daniel <danielogen@gmail.com>
2024-08-11 12:57:54 -0700

    (DOCS): Add code comments and more regex

--- src/PyReprism/languages/java.py
+++ src/PyReprism/languages/java.py
@@ -8,36 +8,106 @@ class Java:
 
     @staticmethod
     def file_extension() -> str:
+        """
+        Return the file extension used for Java files.
+
+        :return: The file extension for Java files.
+        :rtype: str
+        """
         return extension.java
 
     @staticmethod
     def keywords() -> list:
+        """
+        Return a list of Java keywords and built-in functions.
+
+        :return: A list of Java keywords and built-in function names.
+        :rtype: list
+        """
         keyword = 'abstract|continue|for|new|switch|assert|default|goto|package|synchronized|boolean|do|if|private|this|break|double|implements|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return|transient|catch|extends|int|short|try|char|final|interface|static|void|class|finally|long|strictfp|volatile|const|float|native|super|while'.split('|')
         return keyword
 
     @staticmethod
-    def comment_regex():
+    def comment_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify different types of comments and non-comment code in Python source files.
+
+        :return: A compiled regex pattern with named groups to match single-line comments, multiline comments, and non-comment code elements.
+        :rtype: re.Pattern
+        """
         pattern = re.compile(r'(?P<comment>//.*?$|/\*[^*]*\*+(?:[^/*][^*]*\*+)*?/)|(?P<noncomment>[^/]+)', re.DOTALL | re.MULTILINE)
         return pattern
 
     @staticmethod
-    def number_regex():
+    def number_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify numeric literals in Java code.
+
+        :return: A compiled regex pattern to match Java numeric literals, including integers, floats, and complex numbers.
+        :rtype: re.Pattern
+        """
         pattern = re.compile(r'\b0b[01]+\b|\b0x[\da-f]*\.?[\da-fp-]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?[df]?')
         return pattern
 
     @staticmethod
-    def operator_regex():
+    def operator_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify Java operators.
+
+        :return: A compiled regex pattern to match various Java operators and logical keywords.
+        :rtype: re.Pattern
+        """
         pattern = re.compile(r'(^|[^.])(?:\+[+=]?|-[-=]?|!=?|<<?=?|>>?>?=?|==?|&[&=]?|\|[|=]?|\*=?|\/=?|%=?|\^=?|[?:~])')
         return pattern
 
     @staticmethod
-    def keywords_regex():
+    def keywords_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify Java keywords.
+
+        :return: A compiled regex pattern to match Java keywords.
+        :rtype: re.Pattern
+        """
         return re.compile(r'\b(' + '|'.join(Java.keywords()) + r')\b')
 
     @staticmethod
+    def boolean_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify Java boolean literals.
+
+        :return: A compiled regex pattern to match Java boolean literals.
+        :rtype: re.Pattern
+        """
+        return re.compile(r'\b(?:true|false)\b')
+
+    @staticmethod
+    def delimiters_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify Java delimiters.
+
+        :return: A compiled regex pattern to match Java delimiters.
+        :rtype: re.Pattern
+        """
+        return re.compile(r'[()\[\]{}.,:;@<>]')
+
+    @staticmethod
     def remove_comments(source_code: str) -> str:
+        """
+        Remove comments from the provided Java source code string.
+
+        :param str source_code: The Java source code from which to remove comments.
+        :return: The source code with all comments removed.
+        :rtype: str
+        """
         return Java.comment_regex().sub(lambda match: match.group('noncomment') if match.group('noncomment') else '', source_code).strip()
 
     @staticmethod
-    def remove_keywords(source: str):
+    def remove_keywords(source: str) -> str:
+        """
+        Remove all Python keywords from the provided source code string.
+
+        :param str source: The source code string from which to remove Java keywords.
+        :return: The source code string with all Java keywords removed.
+        :rtype: str
+        """
         return re.sub(re.compile(Java.keywords_regex()), '', source).strip()
--- src/PyReprism/languages/python.py
+++ src/PyReprism/languages/python.py
@@ -8,34 +8,98 @@ class Python:
 
     @staticmethod
     def file_extension() -> str:
+        """
+        Return the file extension used for Python files.
+
+        :return: The file extension for Python files.
+        :rtype: str
+        """
         return extension.python
 
     @staticmethod
     def keywords() -> list:
+        """
+        Return a list of Python keywords and built-in functions.
+
+        :return: A list of Python keywords and built-in function names.
+        :rtype: list
+        """
         keyword = 'as|assert|async|await|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|nonlocal|pass|print|raise|return|try|while|with|yield|__import__|abs|all|any|apply|ascii|basestring|bin|bool|buffer|bytearray|bytes|case|callable|chr|classmethod|cmp|coerce|compile|complex|delattr|dict|dir|divmod|enumerate|eval|execfile|file|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|intern|isinstance|issubclass|iter|len|list|locals|long|match|map|max|memoryview|min|next|object|oct|open|ord|pow|property|range|raw_input|reduce|reload|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|unichr|unicode|vars|xrange|zip|True|False|None'.split('|')
         return keyword
 
     @staticmethod
-    def comment_regex():
+    def comment_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify different types of comments and non-comment code in Python source files.
+
+        :return: A compiled regex pattern with named groups to match single-line comments, multiline comments, and non-comment code elements.
+        :rtype: re.Pattern
+        """
         pattern = re.compile(r'(?P<comment>#.*?$)|'r'(?P<multilinecomment1>""".*?""")|'r'(?P<multilinecomment2>\'\'\'.*?\'\'\')|'r'(?P<noncomment>\'(\\.|[^\\\'])*\'|"(\\.|[^\\"])*"|.[^#\'"]*)', re.DOTALL | re.MULTILINE)
         return pattern
 
     @staticmethod
-    def number_regex():
+    def number_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify numeric literals in Python code.
+
+        :return: A compiled regex pattern to match Python numeric literals, including integers, floats, and complex numbers.
+        :rtype: re.Pattern
+        """
         pattern = re.compile(r'(?:\b(?=\d)|\B(?=\.))(?:0[bo])?(?:(?:\d|0x[\da-f])[\da-f]*\.?\d*|\.\d+)(?:e[+-]?\d+)?j?\b', re.IGNORECASE)
         return pattern
 
     @staticmethod
-    def operator_regex():
+    def operator_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify Python operators.
+
+        :return: A compiled regex pattern to match various Python operators and logical keywords.
+        :rtype: re.Pattern
+        """
         pattern = re.compile(r'[-+%=]=?|!=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]|\b(?:or|and|not)\b')
         return pattern
 
     @staticmethod
-    def keywords_regex():
+    def keywords_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify Python keywords.
+
+        :return: A compiled regex pattern to match Python keywords.
+        :rtype: re.Pattern
+        """
         return re.compile(r'\b(' + '|'.join(Python.keywords()) + r')\b')
 
     @staticmethod
+    def boolean_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify Python boolean literals.
+
+        :return: A compiled regex pattern to match Python boolean literals and `None`.
+        :rtype: re.Pattern
+        """
+        return re.compile(r'\b(?:True|False|None)\b')
+
+    @staticmethod
+    def delimiters_regex() -> re.Pattern:
+        """
+        Compile and return a regular expression pattern to identify Python delimiters.
+
+        :return: A compiled regex pattern to match Python delimiters.
+        :rtype: re.Pattern
+        """
+        return re.compile(r'[()\[\]{}.,:;@]')
+
+    @staticmethod
     def remove_comments(source_code: str, isList: bool = False) -> str:
+        """
+        Remove comments from the provided Python source code string.
+
+        :param str source_code: The Python source code from which to remove comments.
+        :param bool isList: (Optional) A flag indicating if the input is a list of source code lines. This parameter is not used in the function logic.
+        :return: The source code with all comments removed.
+        :rtype: str
+        """
         return Python.comment_regex().sub(lambda match: match.group('noncomment') if match.group('noncomment') else '', source_code).strip()
         result = []
         for match in Python.comment_regex().finditer(source_code):
@@ -46,5 +110,12 @@ class Python:
         return ''.join(result)
 
     @staticmethod
-    def remove_keywords(source: str):
+    def remove_keywords(source: str) -> str:
+        """
+        Remove all Python keywords from the provided source code string.
+
+        :param str source: The source code string from which to remove Python keywords.
+        :return: The source code string with all Python keywords removed.
+        :rtype: str
+        """
         return re.sub(re.compile(Python.keywords_regex()), '', source)
